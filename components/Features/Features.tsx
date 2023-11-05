1a046b0a5066ae9db39c3a2facdb383820ec45f6
nik <sud_nik@bk.ru>
2023-11-05 12:15:07 +0300

    redactoring features

--- components/Features/Features.tsx
+++ components/Features/Features.tsx
@@ -7,14 +7,10 @@ const Features = () => {
   return (
     <Container>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 items-end my-24'>
-        {/* <FeaturesItem src='1' />
-        <FeaturesItem src='2' />
-        <FeaturesItem src='3' w='41' />
-        <FeaturesItem src='4' w='56' h='60' />
-        <FeaturesItem src='5' />
-        <FeaturesItem src='6' /> */}
         {featureItems.map((item, i) => {
-          return <FeaturesItem src={`${i}`} w={item.w} h={item.h} key={i} />;
+          return (
+            <FeaturesItem src={`${i + 1}`} w={item.w} h={item.h} key={i} />
+          );
         })}
       </div>
     </Container>
