bc7c6c0409d7d213b4c685d8423a84b342b34d11
nik <sud_nik@bk.ru>
2023-11-20 12:10:05 +0300

    animate intro

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import Container from "@/UI/Container/Container";
-import Button from "@/UI/Button/Button";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 const Intro = () => {
+  useEffect(() => {
+    setSide(false);
+  }, []);
+  const [side, setSide] = useState(true);
   return (
-    <section className='intro h-screen'>
-      <div className='flex justify-center items-center h-full backdrop-blur-md'>
+    <section className='intro h-screen overflow-hidden'>
+      <div
+        className={`${
+          side && "translate-y-52 opacity-0"
+        } flex justify-center items-center h-full backdrop-blur-md transition-all duration-100`}
+      >
         <div className='flex flex-col items-center max-w-4xl my-0 mx-auto'>
-          <h1 className='md:text-7xl text-5xl text-white text-center uppercase select-none font-normal'>
+          <h1 className='md:text-7xl text-5xl text-white  text-center uppercase select-none font-normal'>
             Новохоперский завод растительных масел
           </h1>
         </div>
